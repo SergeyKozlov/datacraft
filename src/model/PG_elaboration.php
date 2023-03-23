cae6e45ec39b0c5265ce7f8991ad4ee577bd30b7
Sergey Kozlov <hello@sergeykozlov.ru>
2023-03-23 12:31:31 +0300

    data craft first commit

---
+++ src/model/PG_elaboration.php
@@ -0,0 +1,27 @@
+<?php
+
+use model\PostgreSQL;
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/nad/model/PostgreSQL.php');
+
+class PG_elaboration extends PostgreSQL
+{
+    public function pgConnect()
+    {
+        $dotenv = Dotenv\Dotenv::createImmutable('/tmp/');
+        $dotenv->load();
+
+        $host = $_ENV['pg_host'];
+        $port = $_ENV['pg_port'];
+        $username = $_ENV['pg_username'];
+        $password = $_ENV['pg_password'];
+        $database = $_ENV['pg_database_el'];
+        try {
+            $conn = pg_pconnect("host=$host port=$port dbname=$database user=$username password=$password") or die("No base connect");
+            return $conn;
+        } catch (Exception $e) {
+            echo 'No DB. ' . $e;
+            return false;
+        }
+    }
+}
